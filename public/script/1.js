8df52cd3707f1cfec681e5610c01cbe2f2b3d99c
I-Lun Chen <newcomer@newcomer.laptop>
2014-04-04 11:33:44 +0800

    git first time on linux

---
+++ public/script/1.js
@@ -0,0 +1,14 @@
+/**
+ * New node file
+ */
+
+$(document).ready(function() {
+	//alert('ready from jquery');
+	$('.navigation a').on('click', function() {
+		alert(this.href);
+		return false;		
+	});
+	
+});
+
+
